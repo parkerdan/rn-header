19506cb609de33c2090b57c47deb2e957872574c
parkerdan <parker.daniel.o@gmail.com>
2016-10-21 16:24:21 -0500

    update to scroll

--- Header.js
+++ Header.js
@@ -2,11 +2,12 @@
 
 import React, { Component, PropTypes } from 'react';
 import {
- StyleSheet,
- Text,
- View,
- TouchableOpacity,
- Dimensions,
+StyleSheet,
+Text,
+View,
+TouchableOpacity,
+Dimensions,
+Animated
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -14,151 +15,182 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 const { height,width } = Dimensions.get('window');
 
 const propTypes = {
- backgroundColor: React.PropTypes.string.isRequired,
- height: React.PropTypes.number.isRequired,
- text: React.PropTypes.string,
- iconLeftName: React.PropTypes.oneOfType([React.PropTypes.bool,React.PropTypes.string]),
- onLeftIconPress: React.PropTypes.func,
- iconRightName: React.PropTypes.oneOfType([React.PropTypes.bool,React.PropTypes.string]),
- onRightIconPress: React.PropTypes.func,
- thirdIconName: React.PropTypes.oneOfType([React.PropTypes.bool,React.PropTypes.string]),
- onThirdIconPress: React.PropTypes.func,
- textStyle: React.PropTypes.object,
- iconSize: React.PropTypes.number,
- iconColor: React.PropTypes.string
+backgroundColor: React.PropTypes.string.isRequired,
+height: React.PropTypes.number.isRequired,
+text: React.PropTypes.string,
+iconLeftName: React.PropTypes.oneOfType([React.PropTypes.bool,React.PropTypes.string]),
+onLeftIconPress: React.PropTypes.func,
+iconRightName: React.PropTypes.oneOfType([React.PropTypes.bool,React.PropTypes.string]),
+onRightIconPress: React.PropTypes.func,
+thirdIconName: React.PropTypes.oneOfType([React.PropTypes.bool,React.PropTypes.string]),
+onThirdIconPress: React.PropTypes.func,
+textStyle: React.PropTypes.object,
+iconSize: React.PropTypes.number,
+iconColor: React.PropTypes.string,
+
+scrollY: React.PropTypes.object,
+scrollVal: React.PropTypes.number
 };
 
 export default class Header extends Component {
 
- renderLeftIcon(){
-   if (this.props.iconLeftName) {
-     return(
-       <TouchableOpacity
-         hitSlop={{top:10,bottom:10,left:10,right:10}}
-         onPress={this.props.onLeftIconPress}
-         style={{
-           flex:1,
-           alignItems: 'center',
-           justifyContent: 'flex-end',
-         }}>
-         <Icon
-           name={this.props.iconLeftName}
-           size={this.props.iconSize}
-           color={this.props.iconColor}/>
-       </TouchableOpacity>
-     )
-   } else {
-     return(
-       <View style={{
-         flex:1,
-         alignItems: 'center',
-         justifyContent: 'flex-end',
-       }}/>
-     )
-   }
+ constructor(props){
+   super(props);
+   this.state = {
+   };
  };
 
- renderThirdIcon(){
-   if (this.props.thirdIconName) {
-     return(
-       <TouchableOpacity
-         hitSlop={{top:10,bottom:10,left:10,right:10}}
-         onPress={this.props.onThirdIconPress}
-         style={{
-           flex:1,
-           alignItems: 'center',
-           justifyContent: 'flex-end',
-         }}>
-         <Icon
-           name={this.props.thirdIconName}
-           size={this.props.iconSize}
-           color={this.props.iconColor}/>
-       </TouchableOpacity>
-     )
-   }
- };
+renderLeftIcon(){
+  if (this.props.iconLeftName) {
+    return(
+      <TouchableOpacity
+        hitSlop={{top:10,bottom:10,left:10,right:10}}
+        onPress={this.props.onLeftIconPress}
+        style={{
+          flex:1,
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+        }}>
+        <Icon
+          name={this.props.iconLeftName}
+          size={this.props.iconSize}
+          color={this.props.iconColor}/>
+      </TouchableOpacity>
+    )
+  } else {
+    return(
+      <View style={{
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+      }}/>
+    )
+  }
+};
 
- renderRightIcon(){
-   if (this.props.thirdIconName && this.props.iconRightName) {
-     return(
-       <View
-         style={{
-           flexDirection:'row',
-           flex:1,
-           alignItems: 'flex-end',
-         }}>
-         <TouchableOpacity
-           style={{flex:1}}
-           hitSlop={{top:10,bottom:10,left:10,right:10}}
-           onPress={this.props.onThirdIconPress}>
-           <Icon
-             name={this.props.thirdIconName}
-             size={this.props.iconSize}
-             color={this.props.iconColor}/>
-         </TouchableOpacity>
-         <TouchableOpacity
-           style={{flex:1}}
-           onPress={this.props.onRightIconPress}>
-           <Icon
-             name={this.props.iconRightName}
-             size={this.props.iconSize}
-             color={this.props.iconColor}/>
-         </TouchableOpacity>
-       </View>
-     )
-   } else if (this.props.iconRightName) {
-     return(
-       <TouchableOpacity
-         onPress={this.props.onRightIconPress}
-         style={{
-           flex:1,
-           alignItems: 'center',
-           justifyContent: 'flex-end',
-         }}>
-         <Icon
-           name={this.props.iconRightName}
-           size={this.props.iconSize}
-           color={this.props.iconColor}/>
-       </TouchableOpacity>
-     )
-   } else {
-     return(
-       <View style={{
-         flex:1,
-         alignItems: 'center',
-         justifyContent: 'flex-end',
-       }}/>
-     )
-   }
- };
+renderThirdIcon(){
+  if (this.props.thirdIconName) {
+    return(
+      <TouchableOpacity
+        hitSlop={{top:10,bottom:10,left:10,right:10}}
+        onPress={this.props.onThirdIconPress}
+        style={{
+          flex:1,
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+        }}>
+        <Icon
+          name={this.props.thirdIconName}
+          size={this.props.iconSize}
+          color={this.props.iconColor}/>
+      </TouchableOpacity>
+    )
+  }
+};
 
- render(){
-   return(
-     <View
-       style={{
-         backgroundColor: this.props.backgroundColor,
-         height: this.props.height,
-         flexDirection: 'row',
-         width: width,
-         paddingBottom: 4,
-       }}>
-       { this.renderLeftIcon() }
-       <View
-         style={{
-           flex:3,
-           alignItems: 'center',
-           justifyContent: 'flex-end',
-         }}>
-         <Text
-           style={this.props.textStyle}
-           numberOfLines={1}>
-           {this.props.text}
-         </Text>
-       </View>
-       { this.renderRightIcon() }
-     </View>
-   )
- };
+renderRightIcon(){
+  if (this.props.thirdIconName && this.props.iconRightName) {
+    return(
+      <View
+        style={{
+          flexDirection:'row',
+          flex:1,
+          alignItems: 'flex-end',
+        }}>
+        <TouchableOpacity
+          style={{flex:1}}
+          hitSlop={{top:10,bottom:10,left:10,right:10}}
+          onPress={this.props.onThirdIconPress}>
+          <Icon
+            name={this.props.thirdIconName}
+            size={this.props.iconSize}
+            color={this.props.iconColor}/>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{flex:1}}
+          onPress={this.props.onRightIconPress}>
+          <Icon
+            name={this.props.iconRightName}
+            size={this.props.iconSize}
+            color={this.props.iconColor}/>
+        </TouchableOpacity>
+      </View>
+    )
+  } else if (this.props.iconRightName) {
+    return(
+      <TouchableOpacity
+        onPress={this.props.onRightIconPress}
+        style={{
+          flex:1,
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+        }}>
+        <Icon
+          name={this.props.iconRightName}
+          size={this.props.iconSize}
+          color={this.props.iconColor}/>
+      </TouchableOpacity>
+    )
+  } else {
+    return(
+      <View style={{
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+      }}/>
+    )
+  }
+};
+
+renderHeight(){
+  if (this.props.scrollY) {
+    return(
+      this.props.scrollY.interpolate({
+        inputRange: [-100,0,200,200],
+        outputRange: [this.props.height,this.props.height, 0, 0]
+      })
+    )
+  } else return this.props.height
+};
+
+renderHeader(){
+  if (this.props.scrollVal < 200) {
+    return(
+      <Animated.View style={{
+        backgroundColor: this.props.backgroundColor,
+        flexDirection: 'row',
+        width: width,
+        paddingBottom: 4,
+        height: this.renderHeight(),
+      }}>
+        { this.renderLeftIcon() }
+        <View
+          style={{
+            flex:3,
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+          }}>
+          <Text
+            style={this.props.textStyle}
+            numberOfLines={1}>
+            {this.props.text}
+          </Text>
+        </View>
+        { this.renderRightIcon() }
+      </Animated.View>
+    )
+  }
+};
+
+render(){
+
+  return(
+    <View>
+      {this.renderHeader()}
+      {this.props.children}
+    </View>
+  )
+};
 };
 
 Header.propTypes = propTypes;
--- README.md
+++ README.md
@@ -1,76 +1,135 @@
 # RN-Header
-<br>
-#### You must have `react-native-vector-icons` installed and linked for this to work
-<br>
-### Uses Font Awesome icons as the icon source
-* `npm install react-native-vector-icons --save`
-* `react-native link react-native-vector-icons`
-* `npm install rn-header --save`
 
-![Two Icons](./TwoIcons.png "Two Icons")
+### Can Use the header as a stand alone header, or a scroll away header.
+
+#### You must have `react-native-vector-icons` installed and linked for this to work
+
+
+### Uses Font Awesome icons as the icon source
+```js
+npm install react-native-vector-icons --save
+react-native link react-native-vector-icons
+npm install rn-header --save
+
+```
+
+
+- To use as a scroll away header takes a little work to set up.
+-  Import `Animated` from react-native
+- Add two values to the component state.  Need a `scrollY` and the `scrollVal` so the Header can respond properly
+
+- Also need a listener on the animated value
+
+- Can use with Listview as well, the set up is Identical.
+
+- just copy/paste this example and you'll get the hang of it 🤓
+
+####  If you do not want the header to scroll away,  do not supply the header with the `scrollY` and `scrollVal`
+
+
+
+![gif](./scrollaway.gif "scroll away")
 
 ```js
+'use strict'
+
 import React, { Component } from 'react';
+import {
+View,
+Text,
+ScrollView,
+Animated
+} from 'react-native';
 
 import Header from 'rn-header';
 
-class HeaderTwoIcons extends Component {
-  render(){
-    return(
-      <Header
-        backgroundColor={'deeppink'}
-        height={50}
-        text={'Heading'}
-        iconLeftName={'bars'}
-        onLeftIconPress={
-          () => console.log('Do Stuff')
-        }
-        iconRightName={'chevron-right'}
-        onRightIconPress={
-          () => console.log('Do other stuff')
-        }
-        // pass in false or just omit the prop
-        thirdIconName={false}
-        textStyle={{
-          color:'yellow',
-          fontSize:20
-        }}
-        iconSize={20}
-        iconColor={'white'}
-      />
-    )
-  };
 
-class HeaderThreeIcons extends Component {
-  render(){
-    return(
-      <Header
-        backgroundColor={'deeppink'}
-        height={50}
-        text={'Heading'}
-        iconLeftName={'bars'}
-        onLeftIconPress={
-          () => console.log('Do Stuff')
-        }
-        iconRightName={'chevron-right'}
-        onRightIconPress={
-          () => console.log('Do other stuff')
-        }
-        thirdIconName={'cubes'}
-        onThirdIconPress={
-          () => console.log('Do other other stuff')
-        }
-        textStyle={{
-          color:'yellow',
-          fontSize:20
-        }}
-        iconSize={20}
-        iconColor={'white'}
-      />
-    )
-    };
+export default class Example extends Component {
+ constructor(props){
+   super(props);
+   this.scrollListener = this.scrollListener.bind(this);
+   this.state = {
+     scrollY: new Animated.Value(0),
+     scrollVal: 0,
+   };
+ };
 
-}
+ componentWillMount(){
+   this.state.scrollY.addListener(this.scrollListener);
+ };
+
+ componentWillUnmount(){
+   this.state.scrollY.removeAllListeners();
+ };
+
+ scrollListener(e){
+   this.setState({scrollVal:e.value})
+ };
+
+ render(){
+   let things = ['Watch','The','Header','Scroll','Away','As','I','Move'].map((opt,i) => {
+     return(
+       <View
+         key={i}
+         style={{
+           marginTop:60,
+           backgroundColor:'deeppink',
+           paddingLeft:20
+         }}>
+         <Text style={{fontSize:33}}>{opt}</Text>
+       </View>
+     )
+   });
+   return(
+     <View style={{
+       flex:1,
+       backgroundColor:'yellow'
+     }}>
+       <Header
+         scrollVal={this.state.scrollVal}
+         scrollY={this.state.scrollY}
+         backgroundColor={'blue'}
+         height={60}
+         text={'Scroll Away'}
+         iconRightName={'cogs'}
+         iconSize={25}
+         iconColor={'white'}
+         onRightIconPress={ this.addItem }
+         textStyle={{
+           fontSize:20,
+           color:'white',
+           fontWeight:'500'
+         }}>
+
+         <View style={{
+           height:200,
+           backgroundColor:'red',
+           alignItems:'center',
+           justifyContent:'center'
+         }}>
+           <Text style={{
+             color:'white',
+             fontSize:23,
+             textAlign: 'center'
+           }}>Watch me stay after the header goes away!</Text>
+         </View>
+       </Header>
+
+       <ScrollView
+         scrollEventThrottle={16}
+         onScroll={
+           Animated.event(
+           [{nativeEvent: {contentOffset: {y:  this.state.scrollY }}}]
+           )
+         }
+
+         {things}
+       </ScrollView>
+     </View>
+   )
+ };
+
+};
 ```
-
-![Two Icons](./ThreeIcons.png "Two Icons")
+![Two Icons](./TwoIcons.png "Two Icons")
+![Three Icons](./ThreeIcons.png "Three Icons")
