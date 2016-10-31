f1cbd40f377d118dd17d050f903b6592fbc52459
parkerdan <parker.daniel.o@gmail.com>
2016-10-31 12:24:58 -0500

    fix choppiness

--- Header.js
+++ Header.js
@@ -10,6 +10,8 @@ Dimensions,
 Animated
 } from 'react-native';
 
+import {once} from 'lodash';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const { height,width } = Dimensions.get('window');
@@ -35,162 +37,191 @@ zIndex: React.PropTypes.number
 
 export default class Header extends Component {
 
- constructor(props){
-   super(props);
-   this.state = {
-   };
- };
+constructor(props){
+  super(props);
+  this.setHeight = once(this.setHeight.bind(this));
+  this.state = {
+
+  };
+};
+
+setHeight(height){
+  this.setHeight({
+    initialHeight: height,
+  })
+};
 
 renderLeftIcon(){
-  if (this.props.iconLeftName) {
-    return(
-      <TouchableOpacity
-        hitSlop={{top:10,bottom:10,left:10,right:10}}
-        onPress={this.props.onLeftIconPress}
-        style={{
-          flex:1,
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-        }}>
-        <Icon
-          name={this.props.iconLeftName}
-          size={this.props.iconSize}
-          color={this.props.iconColor}/>
-      </TouchableOpacity>
-    )
-  } else {
-    return(
-      <View style={{
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-      }}/>
-    )
-  }
+ if (this.props.iconLeftName) {
+   return(
+     <TouchableOpacity
+       hitSlop={{top:10,bottom:10,left:10,right:10}}
+       onPress={this.props.onLeftIconPress}
+       style={{
+         flex:1,
+         alignItems: 'center',
+         justifyContent: 'flex-end',
+       }}>
+       <Icon
+         name={this.props.iconLeftName}
+         size={this.props.iconSize}
+         color={this.props.iconColor}/>
+     </TouchableOpacity>
+   )
+ } else {
+   return(
+     <View style={{
+       flex:1,
+       alignItems: 'center',
+       justifyContent: 'flex-end',
+     }}/>
+   )
+ }
 };
 
 renderThirdIcon(){
-  if (this.props.thirdIconName) {
-    return(
-      <TouchableOpacity
-        hitSlop={{top:10,bottom:10,left:10,right:10}}
-        onPress={this.props.onThirdIconPress}
-        style={{
-          flex:1,
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-        }}>
-        <Icon
-          name={this.props.thirdIconName}
-          size={this.props.iconSize}
-          color={this.props.iconColor}/>
-      </TouchableOpacity>
-    )
-  }
+ if (this.props.thirdIconName) {
+   return(
+     <TouchableOpacity
+       hitSlop={{top:10,bottom:10,left:10,right:10}}
+       onPress={this.props.onThirdIconPress}
+       style={{
+         flex:1,
+         alignItems: 'center',
+         justifyContent: 'flex-end',
+       }}>
+       <Icon
+         name={this.props.thirdIconName}
+         size={this.props.iconSize}
+         color={this.props.iconColor}/>
+     </TouchableOpacity>
+   )
+ }
 };
 
 renderRightIcon(){
-  if (this.props.thirdIconName && this.props.iconRightName) {
-    return(
-      <View
-        style={{
-          flexDirection:'row',
-          flex:1,
-          alignItems: 'flex-end',
-        }}>
-        <TouchableOpacity
-          style={{flex:1}}
-          hitSlop={{top:10,bottom:10,left:10,right:10}}
-          onPress={this.props.onThirdIconPress}>
-          <Icon
-            name={this.props.thirdIconName}
-            size={this.props.iconSize}
-            color={this.props.iconColor}/>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{flex:1}}
-          onPress={this.props.onRightIconPress}>
-          <Icon
-            name={this.props.iconRightName}
-            size={this.props.iconSize}
-            color={this.props.iconColor}/>
-        </TouchableOpacity>
-      </View>
-    )
-  } else if (this.props.iconRightName) {
-    return(
-      <TouchableOpacity
-        onPress={this.props.onRightIconPress}
-        style={{
-          flex:1,
-          alignItems: 'center',
-          justifyContent: 'flex-end',
-        }}>
-        <Icon
-          name={this.props.iconRightName}
-          size={this.props.iconSize}
-          color={this.props.iconColor}/>
-      </TouchableOpacity>
-    )
-  } else {
-    return(
-      <View style={{
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-      }}/>
-    )
-  }
+ if (this.props.thirdIconName && this.props.iconRightName) {
+   return(
+     <View
+       style={{
+         flexDirection:'row',
+         flex:1,
+         alignItems: 'flex-end',
+       }}>
+       <TouchableOpacity
+         style={{flex:1}}
+         hitSlop={{top:10,bottom:10,left:10,right:10}}
+         onPress={this.props.onThirdIconPress}>
+         <Icon
+           name={this.props.thirdIconName}
+           size={this.props.iconSize}
+           color={this.props.iconColor}/>
+       </TouchableOpacity>
+       <TouchableOpacity
+         style={{flex:1}}
+         onPress={this.props.onRightIconPress}>
+         <Icon
+           name={this.props.iconRightName}
+           size={this.props.iconSize}
+           color={this.props.iconColor}/>
+       </TouchableOpacity>
+     </View>
+   )
+ } else if (this.props.iconRightName) {
+   return(
+     <TouchableOpacity
+       onPress={this.props.onRightIconPress}
+       style={{
+         flex:1,
+         alignItems: 'center',
+         justifyContent: 'flex-end',
+       }}>
+       <Icon
+         name={this.props.iconRightName}
+         size={this.props.iconSize}
+         color={this.props.iconColor}/>
+     </TouchableOpacity>
+   )
+ } else {
+   return(
+     <View style={{
+       flex:1,
+       alignItems: 'center',
+       justifyContent: 'flex-end',
+     }}/>
+   )
+ }
 };
 
 renderHeight(){
-  if (this.props.scrollY) {
-    return(
-      this.props.scrollY.interpolate({
-        inputRange: [-100,0,200,200],
-        outputRange: [this.props.height,this.props.height, 0, 0]
-      })
-    )
-  } else return this.props.height
+ if (this.props.scrollY) {
+   return(
+     this.props.scrollY.interpolate({
+       inputRange: [-100,0,200,200],
+       outputRange: [this.props.height,this.props.height, 0, 0]
+     })
+   )
+ } else return this.props.height
 };
 
 renderHeader(){
-  if (this.props.scrollVal < 200 || this.props.scrollVal === undefined) {
-    return(
-      <Animated.View style={{
-        backgroundColor: this.props.backgroundColor,
-        flexDirection: 'row',
-        width: width,
-        paddingBottom: 4,
-        height: this.renderHeight(),
-      }}>
-        { this.renderLeftIcon() }
-        <View
-          style={{
-            flex:3,
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-          }}>
-          <Text
-            style={this.props.textStyle}
-            numberOfLines={1}>
-            {this.props.text}
-          </Text>
-        </View>
-        { this.renderRightIcon() }
-      </Animated.View>
-    )
-  }
+ if (this.props.scrollVal < 200 || this.props.scrollVal === undefined) {
+   return(
+     <Animated.View
+       style={{
+       backgroundColor: this.props.backgroundColor,
+       flexDirection: 'row',
+       width: width,
+       paddingBottom: 4,
+       height: this.renderHeight(),
+     }}>
+       { this.renderLeftIcon() }
+       <View
+         style={{
+           flex:3,
+           alignItems: 'center',
+           justifyContent: 'flex-end',
+         }}>
+         <Text
+           style={this.props.textStyle}
+           numberOfLines={1}>
+           {this.props.text}
+         </Text>
+       </View>
+       { this.renderRightIcon() }
+     </Animated.View>
+   )
+ }
 };
+
+renderTotalHeight(){
+ if (this.props.scrollY) {
+   return(
+     this.props.scrollY.interpolate({
+       inputRange: [-100,0,200,200],
+       outputRange: [this.state.initialHeight,this.state.initialHeight, (this.state.initialHeight - this.props.height), (this.state.initialHeight - this.props.height)]
+     })
+   )
+ }
+};
+
 
 render(){
 
-  return(
-    <View style={{zIndex:this.props.zIndex || 0}}>
-      {this.renderHeader()}
-      {this.props.children}
-    </View>
-  )
+ return(
+   <Animated.View
+     onLayout={
+       (e) => this.setHeight(e.nativeEvent.layout.height)
+     }
+
+     style={{
+       zIndex:this.props.zIndex || 0,
+       height: this.renderTotalHeight()
+     }}>
+     {this.renderHeader()}
+     {this.props.children}
+   </Animated.View>
+ )
 };
 };
 
